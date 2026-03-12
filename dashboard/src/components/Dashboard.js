f09922f391d78561e8d81c6b459f9def6f0d00c5
Somnath474 <somnathgupta474@gmail.com>
2026-03-12 12:00:06 +0530

    fix dashboard build error

--- dashboard/src/components/Dashboard.js
+++ dashboard/src/components/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Apps from "./Apps";
 import Funds from "./Funds";
@@ -12,7 +12,6 @@ import WatchList from "./WatchList";
 import { GeneralContextProvider } from "./GeneralContext";
 
 const Dashboard = () => {
-  const navigate = useNavigate();
   const [isReady, setIsReady] = useState(false);
 
   useEffect(() => {
